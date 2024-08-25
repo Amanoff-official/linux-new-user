1248affc02322b67664c7e184b18225c4dbddddb
Amanoff-official <hackercompany08@gmail.com>
2024-08-25 17:40:38 +0500

    Delete new.sh

--- new.sh
+++
@@ -1,9 +0,0 @@
-username="Admin" # Täze adam
-password="Admin" # Täze Kod
-
-# Täze adam we Kod sohranit etýä
-sudo adduser --gecos "" --disabled-password "$username"
-echo "$username:$password" | sudo chpasswd
-
-# Root berilýä
-sudo usermod -aG sudo "$username"
